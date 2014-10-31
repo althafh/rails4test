cd10c04897756cbad850a5ece83c0ae541a31e49
Althaf Hameez <althaf.hameez@grabtaxi.com>
2014-11-01 02:35:21 +0800

    fix wrong route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails4test::Application.routes.draw do
-  get '*path', to: 'home#index'
+  get '*path', to: 'homes#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
