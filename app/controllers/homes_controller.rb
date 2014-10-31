544a23c107728e1e8a005670902fd7e2c1c4e881
Althaf Hameez <althaf.hameez@grabtaxi.com>
2014-11-01 02:30:00 +0800

    show rails version

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,5 @@
 class HomesController < ApplicationController
   def index
-    return render text: '{}', status: :ok, content_type: 'application/json'
+    return render text: "Rails Version: #{Rails.version}", status: :ok, content_type: 'application/json'
   end 
 end
